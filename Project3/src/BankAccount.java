5e2c486e283808f8b3a2acdb51ecfd58ba96bdbd
Álvaro Pereira <alvarozord@gmail.com>
2026-08-18 21:32:38 -0300

    Fix name for displayDetails Class

--- Project3/src/BankAccount.java
+++ Project3/src/BankAccount.java
@@ -2,8 +2,15 @@ public class BankAccount {
 
     // declaração de atributos/variáveis de instância
     int id;
+    //valor padrão é 0, Java não tem lixo de memória
+
     String clientName;
+    //como String é uma classe, o valor dela é null.
+
     double balance;
+    //para double o valor é 0.
+
+    //quando tem-se o tipo boolean, o valor padrão é false.
     
     // adicionando o comportamente = métodos
     public void deposit(double amount){
@@ -14,7 +21,7 @@ public class BankAccount {
         if(balance >= amount)
             balance -= amount;            
     }
-    public void displayDerails(){
+    public void displayDetails(){
         System.out.println("Dados da conta:");
         System.out.println("Código: " + id);
         System.out.println("Nome do Cliente: " + clientName);
