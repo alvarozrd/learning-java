08607a9d442872d305501a96819381c89dc2a0a4
Álvaro Pereira <alvarozord@gmail.com>
2026-08-19 20:35:08 -0300

    Add private constructos for BankAccount

--- Project3/src/App.java
+++ Project3/src/App.java
@@ -1,16 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        BankAccount account = new BankAccount(); 
+        BankAccount account = new BankAccount(2,"Cláudi Leite",400); 
+
         //account é uma variável do tipo referência - podendo ser chamado de objeto.
         //new é o nome da classe com (), onde é chamado como default o construtor da classe.
 
+
+        account.displayDetails();
+        account.setID(1);
+        account.setClientName("Roberto Carlos");
+        account.setBalance(1000);
+        System.out.println("Código: " + account.getID());
+        System.out.println("Nome do cliente: " + account.getClientName());
+        System.out.println("Saldo: R$ %.2f\n" + account.getBalance());
         account.deposit(1000);
         account.displayDetails();
         account.withdraw(500);
         account.displayDetails();
         
-        BankAccount account2 = new BankAccount();
+        BankAccount account2 = new BankAccount(3, "Billie Elish");
         account2.deposit(300);
         account2.displayDetails();
+        
+        BankAccount account3 = new BankAccount(2, null);
+        account3.displayDetails();
+
     }
 }
--- Project3/src/BankAccount.java
+++ Project3/src/BankAccount.java
@@ -1,17 +1,47 @@
 public class BankAccount {
 
     // declaração de atributos/variáveis de instância
-    int id;
     //valor padrão é 0, Java não tem lixo de memória
-
-    String clientName;
     //como String é uma classe, o valor dela é null.
-
-    double balance;
     //para double o valor é 0.
-
     //quando tem-se o tipo boolean, o valor padrão é false.
+
+    private int id;
+    private String clientName;
+    private double balance;
     
+    //métodos construtores
+
+    public BankAccount (int id, String clientName){
+        this(id, clientName, 0);
+    }
+    public BankAccount(int id, String clientName, double balance){
+        this.id = id;
+        this.clientName = clientName;
+        this.balance = balance;
+    }
+    public void setID (int id){
+        this.id = id;
+    }
+    public int getID (){
+        return id;
+    }
+
+    public void setClientName(String clientName){
+        this.clientName = clientName;
+    }
+    public String getClientName(){
+        return clientName;
+    }
+
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+    public double getBalance(){
+        return balance;
+    }
+
+
     // adicionando o comportamente = métodos
     public void deposit(double amount){
         balance += amount; 
