f0ac63ece5f0eb1a694ed5da992e5655fb530ed9
Álvaro Pereira <alvarozord@gmail.com>
2026-09-08 17:30:59 -0300

    Add rough task comments

--- list-07/src/App.java
+++ list-07/src/App.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        // criando o scanner e a lista de tarefas
         Scanner scanner = new Scanner(System.in);
         ArrayList<Task> tasks = new ArrayList<Task>();
         int option;
 
+        // menu do programa
         do {
             System.out.println("\nMENU DE TAREFAS");
             System.out.println("1 - Adicionar uma tarefa");
@@ -19,6 +21,7 @@ public class App {
 
             switch (option) {
                 case 1:
+                    // adicionando uma nova tarefa
                     System.out.print("Digite o código da tarefa: ");
                     int code = scanner.nextInt();
                     scanner.nextLine();
@@ -33,12 +36,14 @@ public class App {
                     break;
 
                 case 2:
+                    // removendo uma tarefa pelo código
                     System.out.print("Digite o código da tarefa que deseja remover: ");
                     int codeToRemove = scanner.nextInt();
                     scanner.nextLine();
 
                     boolean removed = false;
 
+                    // procurando a tarefa dentro da lista
                     for (int i = 0; i < tasks.size(); i++) {
                         if (tasks.get(i).getCode() == codeToRemove) {
                             tasks.remove(i);
@@ -55,6 +60,7 @@ public class App {
                     break;
 
                 case 3:
+                    // mostrando as tarefas cadastradas
                     if (tasks.isEmpty()) {
                         System.out.println("Nenhuma tarefa cadastrada.");
                     } else {
@@ -69,6 +75,7 @@ public class App {
                     break;
 
                 case 4:
+                    // saindo do programa
                     System.out.println("Programa encerrado.");
                     break;
 
@@ -76,6 +83,7 @@ public class App {
                     System.out.println("Opção inválida.");
                     break;
             }
+        // repete o menu até o usuário escolher sair
         } while (option != 4);
 
         scanner.close();
--- list-07/src/Task.java
+++ list-07/src/Task.java
@@ -1,24 +1,30 @@
 public class Task {
+    // atributos da tarefa
     private int code;
     private String title;
 
+    // construtor para criar a tarefa com código e título
     public Task(int code, String title) {
         setCode(code);
         setTitle(title);
     }
 
+    // pega o código da tarefa
     public int getCode() {
         return code;
     }
 
+    // altera o código da tarefa
     public void setCode(int code) {
         this.code = code;
     }
 
+    // pega o título da tarefa
     public String getTitle() {
         return title;
     }
 
+    // altera o título da tarefa
     public void setTitle(String title) {
         this.title = title;
     }
