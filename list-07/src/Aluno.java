266c40b4bc840f4e99cff5ecbd8df96c85d4e9a1
Álvaro Pereira <alvarozord@gmail.com>
2026-09-08 22:24:28 -0300

    Implement student registration case

--- list-07/src/Aluno.java
+++ list-07/src/Aluno.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
@@ -7,15 +8,26 @@ public class Aluno {
     public static void main(String[] args) {
 
         int option;
-        ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+        List<Classmate> estudantes = new ArrayList<>();
+
+
 
         do{
-            option = Integer.parseInt(JOptionPane.showInputDialog("\nMENU DE TAREFAS\n1 - Cadastrar Aluno\n2 - Remover uma tarefa\n3 - Listar todas as tarefas\n4 - Sair do programa\nEscolha uma opção: "));
+            option = Integer.parseInt(JOptionPane.showInputDialog("\nMENU DE TAREFAS\n1 - Cadastrar Aluno\n2 - Alunos aprovados\n3 - Listar todas as tarefas\n4 - Sair do programa\nEscolha uma opção: "));
 
             switch (option){
                 case 1: {
+                    String prontuario = JOptionPane.showInputDialog("Informe o prontuário: ");
+                    String nome = JOptionPane.showInputDialog("Informe o nome: ");
+                    double nota = Double.parseDouble(JOptionPane.showInputDialog("Informe a nota: "));
 
+                    Classmate aluno = new Classmate(prontuario, nome, nota);
+                    estudantes.add(aluno);
+                    break;
                 } 
+                case 2: {
+
+                }
             }
         }
         while (option != 4);
