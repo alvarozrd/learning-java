8abb442a90616274ef1c899abb5e71d3543e69db
Álvaro Pereira <alvarozord@gmail.com>
2026-09-22 21:42:56 -0300

    Project-CHECKOUT

---
+++ CHECKOUT/src/br/edu/ifspcjo/ads/web2/checkout/model/Authorizable.java
@@ -0,0 +1,7 @@
+package br.edu.ifspcjo.ads.web2.checkout.model;
+
+public interface Authorizable {
+
+    double getTotal();
+    
+}
---
+++ CHECKOUT/src/br/edu/ifspcjo/ads/web2/checkout/model/Printable.java
@@ -0,0 +1,8 @@
+package br.edu.ifspcjo.ads.web2.checkout.model;
+
+public interface Printable {
+
+        String getPageHeader(); 
+        String GerPageBody();
+        
+}
